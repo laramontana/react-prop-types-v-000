29638d49bcecd8524b6976a19e0e33dc60ca40f0
laramontana <larabrightside@gmail.com>
2017-11-30 01:05:41 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-// Code Goes Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Order from './components/Order';
+
+ReactDOM.render(
+  <Order
+  scoops = { ["chocolate", "vanila"] }
+  orderInfo = { {customerName: "Lara", orderedAt: 1511992840189 } }
+   />,
+  document.getElementById('root')
+);
